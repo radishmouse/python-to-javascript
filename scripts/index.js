9cd23bcba8cba1c8efcec21b7b027d9be5c89ce9
Chris Aquino <chrisaquino@radishmouse.local>
2019-12-02 09:54:22 -0500

    adds variables and hello world

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+// This is a comment
+// This is another comment.
+
+/*
+What?
+It looks like a CSS comment!
+Yasss.
+*/
+
+// Variables! (Storing single pieces of info.)
+// turkey_legs_consumed = 3
+var turkeyLegsConsumed = 3;
+// 1. Prefix your variables the first time you use them.
+// 2. Use camelCase!
+// 3. Terminate each line with `;`
+
+// `var` was the original way to declare variables.
+// `let` is better because:
+// It gives you more control. (You can limit its use to a specific if/else or a specific loop.)
+let turkeysCooked = 2;
+
+// `const` is like `let`, but:
+// - You must assign it when you declare it.
+// - You can never reassign it.
+const numberOfTurkeysICanEat = 1;
+
+// How to use these keywords:
+// 1. By default, create your variables using `const`
+// 2. If you find out that you need to reassign it, change it to `let`.
+
+// print("Hello World!")
+console.log("Hello World!");
+
+// print(turkeysCooked)
+console.log(turkeysCooked);
